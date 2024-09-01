4dad9a02c24e15cd3efbe375e458421df18fcb01
Muhammad Usman <usmanlive399@gmail.com>
2024-09-01 12:14:42 +0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Q-7-8 of 45 days codding challenge
+# q-7-8 ts/js
---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log(5 + 3);
+console.log(11 - 3);
+console.log(4 * 2);
+console.log(16 / 2);
+// ...............And........
+console.log(5 + 5);
+console.log(20 - 10);
+console.log(5 * 2);
+console.log(100 / 10);
